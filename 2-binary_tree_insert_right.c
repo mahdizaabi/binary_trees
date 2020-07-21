14198983f81dc33df949c671a109650a175d6543
karim bahri <karim-bahri-18@outlook.fr>
2020-07-21 23:49:15 +0000

    insert right

---
+++ 2-binary_tree_insert_right.c
@@ -0,0 +1,34 @@
+#include "binary_trees.h"
+
+/**
+ * binary_tree_insert_left - insert left
+ * @parent: parent node
+ * @value: value of the node
+ * Return: node
+ */
+binary_tree_t *binary_tree_insert_right(binary_tree_t *parent, int value)
+{
+	binary_tree_t *node;
+
+	node = malloc(sizeof(binary_tree_t));
+
+	if (!node)
+		return (NULL);
+
+	node->n = value;
+	node->parent = parent;
+	node->left = NULL;
+
+	if (!parent->right)
+	{
+		parent->right = node;
+		node->right = NULL;
+	}
+	else
+	{
+		node->right = parent->right;
+		parent->right->parent = node;
+		parent->right = node;
+	}
+	return (node);
+}
