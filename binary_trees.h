b0bae041660a399e39b59350a2ea4aaa677cc778
karim bahri <karim-bahri-18@outlook.fr>
2020-07-22 16:52:57 +0000

    advanced

--- binary_trees.h
+++ binary_trees.h
@@ -44,4 +44,6 @@ size_t binary_tree_size(const binary_tree_t *tree);
 size_t binary_tree_leaves(const binary_tree_t *tree);
 int binary_tree_balance(const binary_tree_t *tree);
 int _binary_tree_height(const binary_tree_t *tree);
+binary_tree_t *binary_trees_ancestor(const binary_tree_t *first,
+				     const binary_tree_t *second);
 #endif
