1c48e1d431516a3281423ace26a808cd16ef313a
mahdizaabi <mahdi.zaabi@gmail.com>
2020-07-22 14:15:38 +0000

    adding_file

--- 100-binary_trees_ancestor.c
+++ 100-binary_trees_ancestor.c
@@ -1,6 +1,28 @@
 #include "binary_trees.h"
 
 /**
+ * binary_tree_depth - compute the depth of BT recursively
+ * @tree: pointer to the node of ze tree
+ *
+ * Return: depth of the tree
+ */
+size_t binary_tree_depth(const binary_tree_t *tree)
+{
+	size_t a = 0;
+
+	if (tree == NULL)
+		return (0);
+	while (tree->parent)
+	{
+		tree = tree->parent;
+		a += 1;
+	}
+	return (a);
+}
+
+
+
+/**
  * binary_tree_insert_right - insert left
  * @parent: parent node
  * @value: value of the node
@@ -18,7 +40,7 @@ binary_tree_t *binary_trees_ancestor(const binary_tree_t *first, const binary_tr
 	tempx = first;
 	tempy = second;
 
-	if (binary_tree_depth(first) > binary_tree_depth(second))
+	if (binary_tree_depth(first) >= binary_tree_depth(second))
 		{
 			while(first)
 			{
@@ -46,7 +68,6 @@ binary_tree_t *binary_trees_ancestor(const binary_tree_t *first, const binary_tr
 				}
 				second = second->parent;
 			}
-			
 		}
 
 	return (NULL);
