20bf3b862d086326230a6a18251d6d57ae6c80eb
crxs10 <59511107+mahdizaabi@users.noreply.github.com>
2020-07-22 14:20:33 +0200

    Update 2-binary_tree_insert_right.c

--- 2-binary_tree_insert_right.c
+++ 2-binary_tree_insert_right.c
@@ -9,9 +9,7 @@
 binary_tree_t *binary_tree_insert_right(binary_tree_t *parent, int value)
 {
 	binary_tree_t *node;
-	
-	if (parent == NULL)
-		return (NULL);
+
 	node = malloc(sizeof(binary_tree_t));
 
 	if (!node)
