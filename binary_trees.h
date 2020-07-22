dc9888bdda382eac383ae5b3a4c3d25a2b49cf39
karim bahri <karim-bahri-18@outlook.fr>
2020-07-22 16:37:03 +0000

    betty

--- binary_trees.h
+++ binary_trees.h
@@ -14,10 +14,10 @@
  */
 struct binary_tree_s
 {
-    int n;
-    struct binary_tree_s *parent;
-    struct binary_tree_s *left;
-    struct binary_tree_s *right;
+	int n;
+	struct binary_tree_s *parent;
+	struct binary_tree_s *left;
+	struct binary_tree_s *right;
 };
 
 typedef struct binary_tree_s binary_tree_t;
@@ -42,4 +42,6 @@ void binary_tree_postorder(const binary_tree_t *tree, void (*func)(int));
 size_t binary_tree_depth(const binary_tree_t *tree);
 size_t binary_tree_size(const binary_tree_t *tree);
 size_t binary_tree_leaves(const binary_tree_t *tree);
+int binary_tree_balance(const binary_tree_t *tree);
+int _binary_tree_height(const binary_tree_t *tree);
 #endif
