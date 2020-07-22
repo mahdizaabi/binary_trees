dc6a252f80530ef2196f0251e52b6f469ddfe414
mahdizaabi <mahdi.zaabi@gmail.com>
2020-07-22 14:23:36 +0000

    adding_file

--- 100-binary_trees_ancestor.c
+++ 100-binary_trees_ancestor.c
@@ -20,56 +20,51 @@ size_t binary_tree_depth(const binary_tree_t *tree)
 	return (a);
 }
 
-
-
 /**
- * binary_tree_insert_right - insert left
- * @parent: parent node
- * @value: value of the node
- * Return: node
+ * binary_trees_ancestor - insert left
+ * @first: pointer to ze first node
+ * @second: pointer to ze second node
+ * Return: node of ze ancestor
  */
 
-binary_tree_t *binary_trees_ancestor(const binary_tree_t *first, const binary_tree_t *second)
+binary_tree_t *binary_trees_ancestor(const binary_tree_t *first,
+				     const binary_tree_t *second)
 {
-
 	const binary_tree_t *tempx;
 	const binary_tree_t *tempy;
 
 	if (!first || !second)
-		return NULL;
+		return (NULL);
 	tempx = first;
 	tempy = second;
-
 	if (binary_tree_depth(first) >= binary_tree_depth(second))
+	{
+		while (first)
 		{
-			while(first)
+			second = tempy;
+			while (second)
 			{
-				second = tempy;
-				while(second)
-				{
-					if (second == first)
-						return ((binary_tree_t *)second);
-					second = second->parent;
-				}
-				first = first->parent;
-			}
-
-		}
-	if (binary_tree_depth(second) > binary_tree_depth(first))
-		{
-			while(second)
-			{
-				first = tempx;
-				while(first)
-				{
-					if (first == second)
-						return ((binary_tree_t *)first);
-					first = first->parent;
-				}
+				if (second == first)
+					return ((binary_tree_t *)second);
 				second = second->parent;
 			}
+			first = first->parent;
 		}
 
+	}
+	if (binary_tree_depth(second) > binary_tree_depth(first))
+	{
+		while (second)
+		{
+			first = tempx;
+			while (first)
+			{
+				if (first == second)
+					return ((binary_tree_t *)first);
+				first = first->parent;
+			}
+			second = second->parent;
+		}
+	}
 	return (NULL);
-
 }
