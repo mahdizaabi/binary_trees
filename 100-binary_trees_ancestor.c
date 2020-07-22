242f2c387eff26f6aa20e4ffbd323a4c4a81f9a0
mahdizaabi <mahdi.zaabi@gmail.com>
2020-07-22 14:11:24 +0000

    adding_file

--- 100-binary_trees_ancestor.c
+++ 100-binary_trees_ancestor.c
@@ -10,13 +10,19 @@
 binary_tree_t *binary_trees_ancestor(const binary_tree_t *first, const binary_tree_t *second)
 {
 
+	const binary_tree_t *tempx;
+	const binary_tree_t *tempy;
+
 	if (!first || !second)
 		return NULL;
+	tempx = first;
+	tempy = second;
 
 	if (binary_tree_depth(first) > binary_tree_depth(second))
 		{
 			while(first)
 			{
+				second = tempy;
 				while(second)
 				{
 					if (second == first)
@@ -31,6 +37,7 @@ binary_tree_t *binary_trees_ancestor(const binary_tree_t *first, const binary_tr
 		{
 			while(second)
 			{
+				first = tempx;
 				while(first)
 				{
 					if (first == second)
