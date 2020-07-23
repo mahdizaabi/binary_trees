19b21c22ed359d838ab91e44695cdd0ea038fe17
mahdizaabi <mahdi.zaabi@gmail.com>
2020-07-23 23:05:21 +0000

    adding_file

--- 110-binary_tree_is_bst.c
+++ 110-binary_tree_is_bst.c
@@ -1,10 +1,14 @@
 #include "binary_trees.h"
 #include "limits.h"
+/**
+ * check_bst- prufen ob Wert des Nodes geringer/grösser:links/recht subtreee
+ * @root: pointer to the root of ze tree
+ * @min: lower bound
+ * @max: upper bound
+ * Return: 1 wenn das test ergolgreich ist, 0 wenn nicht
+ */
 
-
-
-
-int check_bst(binary_tree_t *root, int min, int max)
+int check_bst(const binary_tree_t *root, int min, int max)
 {
 
 	if (root == NULL)
@@ -13,11 +17,13 @@ int check_bst(binary_tree_t *root, int min, int max)
 		return (0);
 	return (check_bst(root->left, min, root->n) &&
 	check_bst(root->right, root->n, max));
-		
-
 }
-
-
+/**
+ * binary_tree_is_bst - prüfen ob es ein binary search tree ist
+ * @tree: Zeiger zu dem Root des Baumes.
+ *
+ * Return: 1 wenn ja , wenn nicht 0
+ */
 
 int binary_tree_is_bst(const binary_tree_t *tree)
 {
