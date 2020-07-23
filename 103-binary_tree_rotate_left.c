a2512a45b00a79af3e7939ef52f47748063227ab
mahdizaabi <mahdi.zaabi@gmail.com>
2020-07-23 13:57:47 +0000

    adding_file

--- 103-binary_tree_rotate_left.c
+++ 103-binary_tree_rotate_left.c
@@ -1,15 +1,31 @@
 #include "binary_trees.h"
-
+/**
+ * binary_tree_rotate_left - rotate a tree left
+ * @tree: pointer to ze rooth of ze tree
+ * Return: pointer to ze rotated root of ze tree
+ * Ps: CODE IS NOT DRY and extended for my learning purposes
+ */
 
 binary_tree_t *binary_tree_rotate_left(binary_tree_t *tree)
 {
 	if (tree == NULL)
 		return (NULL);
-	
-	tree->parent = tree->right;
-	tree->parent->parent = NULL;
-	tree->right->left = tree;
-	tree->right = NULL;
-	return(tree->parent);
+
+	if (tree->right->left == NULL)
+	{
+		tree->parent = tree->right;
+		tree->parent->parent = NULL;
+		tree->right->left = tree;
+		tree->right = NULL;
+	}
+	else if (tree->right->left)
+	{
+		tree->parent = tree->right;
+		tree->right = tree->right->left;
+		tree->parent->left = tree;
+		tree->parent->parent = NULL;
+		tree->right->parent = tree;
+	}
+	return (tree->parent);
 }
 
