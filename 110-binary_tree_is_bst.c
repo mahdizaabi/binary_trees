8dee8ffcd9e147d37e8829851ed5ab304f501ed5
mahdizaabi <mahdi.zaabi@gmail.com>
2020-07-23 23:24:08 +0000

    adding_file

--- 110-binary_tree_is_bst.c
+++ 110-binary_tree_is_bst.c
@@ -35,7 +35,9 @@ int binary_tree_is_bst(const binary_tree_t *tree)
 	{
 		if (check_bst(tree, INT_MIN, INT_MAX))
 			return (1);
+		else
+			return (0);
 	}
-	
+
 	return (check_bst(tree, INT_MIN, INT_MAX));
 }
