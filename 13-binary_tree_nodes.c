afedbd475868fd970177ab05ec630ef7118820b4
karim bahri <karim-bahri-18@outlook.fr>
2020-07-24 02:31:50 +0000

    nodes

--- 13-binary_tree_nodes.c
+++ 13-binary_tree_nodes.c
@@ -11,6 +11,6 @@ size_t binary_tree_nodes(const binary_tree_t *tree)
 		return (0);
 
 	if (tree->left || tree->right)
-		return (1 + binary_tree_nodes(tree->left) + binary_tree_nodes(tree->left));
-	return (binary_tree_nodes(tree->left) + binary_tree_nodes(tree->left));
+		return (1 + binary_tree_nodes(tree->left) + binary_tree_nodes(tree->right));
+	return (binary_tree_nodes(tree->left) + binary_tree_nodes(tree->right));
 }
