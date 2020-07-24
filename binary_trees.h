5d2072f71a6a41fbf5500d7a30927182fe0d7e41
mahdizaabi <mahdi.zaabi@gmail.com>
2020-07-24 03:09:06 +0000

    adding_file

--- binary_trees.h
+++ binary_trees.h
@@ -57,4 +57,5 @@ binary_tree_t *binary_tree_uncle(binary_tree_t *node);
 size_t binary_tree_nodes(const binary_tree_t *tree);
 bst_t *bst_insert(bst_t **tree, int value);
 bst_t *array_to_bst(int *array, size_t size);
+bst_t *bst_search(const bst_t *tree, int value);
 #endif
