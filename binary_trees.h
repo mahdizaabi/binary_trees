35243b571d5b3480010e959691fd1e5dc8a6ecf5
karim bahri <karim-bahri-18@outlook.fr>
2020-07-24 02:26:25 +0000

    tree nodes

--- binary_trees.h
+++ binary_trees.h
@@ -54,5 +54,5 @@ binary_tree_t *binary_tree_sibling(binary_tree_t *node);
 binary_tree_t *binary_tree_rotate_right(binary_tree_t *tree);
 int binary_tree_is_bst(const binary_tree_t *tree);
 binary_tree_t *binary_tree_uncle(binary_tree_t *node);
-
+size_t binary_tree_nodes(const binary_tree_t *tree);
 #endif
