a1d290111fb3f2a5ed895a59ed886cf7c1ea3141
mahdizaabi <mahdi.zaabi@gmail.com>
2020-07-24 02:51:49 +0000

    adding_file

--- binary_trees.h
+++ binary_trees.h
@@ -55,4 +55,5 @@ binary_tree_t *binary_tree_rotate_right(binary_tree_t *tree);
 int binary_tree_is_bst(const binary_tree_t *tree);
 binary_tree_t *binary_tree_uncle(binary_tree_t *node);
 bst_t *bst_insert(bst_t **tree, int value);
+bst_t *array_to_bst(int *array, size_t size);
 #endif
